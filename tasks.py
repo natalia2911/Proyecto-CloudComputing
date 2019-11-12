4dd37869686838017884d43ef2348870be11bb4a
natalia2911 <nataliamartir@correo.ugr.es>
2019-11-12 10:21:18 +0100

    Añadidos comentarios close #37

--- tasks.py
+++ tasks.py
@@ -1,8 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from invoke import task,run
 
+# Tarea que nos instala dependencias
 @task
 def build(n):
     n.run("pip install -r requirements.txt")
+
+# Tarea que ejecuta los test
 @task
 def test(n):
     with n.cd('tests/'):
