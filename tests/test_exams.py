584f3bdd157e56a75f01ba7b56c6499f6219f53a
natalia2911 <nataliamartir@correo.ugr.es>
2019-11-27 10:08:33 +0100

    Modificado el test de la gestión de examenes

--- tests/test_exams.py
+++ tests/test_exams.py
@@ -6,14 +6,7 @@ sys.path.append('src')
 from Exams import Exams
 from SubjectList import SubjectList
 
-result = {
-    """
-    Examen de la asignatura CC
-
-    Fecha: 01/02/2020
-
-    Lugar: Sala de usos multiples"""
-}
+result = "Examen de la asignatura: CC\n\nFecha: 01/02/2020\n\nLugar: Sala de usos multiples\n\n"
 
 subject_list = SubjectList()
 
@@ -24,22 +17,31 @@ def test_failed_subject():
 
     with pytest.raises(ValueError):
         exam = Exams(failed_subject,date,subject_list,place)
-        assert exam.get_subject_exam == result
+        assert exam.get_subject_exam() == result
 
 def test_failed_date():
     subject = "CC"
     failed_date = "23/23/23"
     place = "Sala de usos multiples"
-
-    with pytest.raises(ValueError):
-        exam = Exams(subject,failed_date,subject_list,place)
-        assert exam.get_subject_exam == result
+    exam = Exams(subject,failed_date,subject_list,place)
+    with pytest.raises(TypeError) as e:
+        raise TypeError(result)
+        assert str(exam.get_subject_exam()) != result
 
 def test_failed_place():
     subject = "CC"
     date = "01/02/2020"
     failed_place = "En mi casa"
 
-    with pytest.raises(ValueError):
+    with pytest.raises(TypeError) as e:
+        raise TypeError(result)
         exam = Exams(subject,date,subject_list,failed_place)
+
+if __name__ == "__main__":
+    subject = "CC"
+    failed_date = "23/23/23"
+    place = "Sala de usos multiples"
+    exam = Exams(subject,failed_date,subject_list,place)
+    print(exam.get_subject_exam())
+    print(result)
 
