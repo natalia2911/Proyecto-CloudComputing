948858598310929aefd9729f30d4a2308ffe19f3
natalia2911 <nataliamartir@correo.ugr.es>
2020-01-03 16:02:16 +0100

    Modificado task.py

--- tasks.py
+++ tasks.py
@@ -20,3 +20,18 @@ def test(n):
 def codecov(n):
     with n.cd('tests/'):
         n.run("codecov")
+
+#Tarea para iniciar el microservicio, debido a que ahora
+# solo tenemos uno lo que hacemos es que no implementamos la 
+# opción de poner más que uno, el puerto por defecto es el 8080
+# y podremos variar el número de workers.
+
+@task
+def run(n,workers=4):
+    with n.cd('src/'):
+        n.run("gunicorn students-rest:app --threads=2 -w "+str(workers)+"  --bind 0.0.0.0:8080")
+
+# Tarea para parar el microservicio
+@task
+def stop(n):
+    n.run("pkill gunicorn")
